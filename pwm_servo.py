51e4e6fb41d68200fd202efe75284ef09e64ff29
Sean Voeller <sean.voeller@gmail.com>
2023-08-15 11:22:02 -0700

    add infinite loop; calculate duty cycle per range that seems to be supproted by the servo I'm working with

--- pwm_servo.py
+++ pwm_servo.py
@@ -5,6 +5,11 @@ from time import sleep
 
 PWM_PIN = 18
 FREQUENCY_HZ = 50
+MIN_DUTY_CYCLE = 2
+MAX_DUTY_CYCLE = 12
+
+def calculate_duty_cycle(percent):
+    return (percent/100 * (MAX_DUTY_CYCLE - MIN_DUTY_CYCLE)) + MIN_DUTY_CYCLE
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PWM_PIN, GPIO.OUT)
@@ -13,9 +18,12 @@ pwm = GPIO.PWM(PWM_PIN, FREQUENCY_HZ)
 pwm.start(0)
 
 try:
-    pwm_percent = float(input('PWM % '))
-    pwm.ChangeDutyCycle(pwm_percent)
-    sleep(.1)
+    while True:
+        pwm_percent = float(input('PWM % '))
+        duty_cycle = calculate_duty_cycle(pwm_percent)
+        print(duty_cycle)
+        pwm.ChangeDutyCycle(duty_cycle)
+        sleep(.1)
 except KeyboardInterrupt:
     print('bye')
 
