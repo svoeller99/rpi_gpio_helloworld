db522f61a733481360fa9c803075254bb62ec0e4
Sean Voeller <sean.voeller@gmail.com>
2023-08-16 14:45:50 -0700

    add loop; add C to F conversion

--- temp_humidity_detector.py
+++ temp_humidity_detector.py
@@ -4,16 +4,22 @@ import RPi.GPIO as GPIO
 import dht11
 import time
 
+def celcius_to_fahrenheit(celcius):
+    return (celcius * 9/5) + 32
+
 GPIO.setmode(GPIO.BCM)
 
 DHT_PIN = 17
 dht = dht11.DHT11(pin = DHT_PIN)
 
 try:
-    reading = dht.read()
-    if reading.is_valid():
-        print(f"Temperature is: {reading.temperature}. Humidity is {reading.humidity}.")
-    time.sleep(.2)
+    while True:
+        reading = dht.read()
+        if reading.is_valid():
+            celcius = reading.temperature
+            fahrenheit = celcius_to_fahrenheit(celcius)
+            print(f"Temperature (celcius) is: {celcius}. Temperature (fahrenheit) is {fahrenheit}. Humidity is {reading.humidity}.")
+        time.sleep(.2)
 except KeyboardInterrupt:
     print('bye')
 
