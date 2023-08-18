661100a2617f810788924771de6d7f2d72db5a7c
Sean Voeller <sean.voeller@gmail.com>
2023-08-18 14:59:44 -0700

    fix

--- photoresistor.py
+++ photoresistor.py
@@ -10,8 +10,7 @@ ADC0834.setup()
 try:
     while True:
         reading = ADC0834.getResult(0)
-        if reading.is_valid():
-            print(reading)
+        print(reading)
         sleep(.2)
 except KeyboardInterrupt:
     print('bye')
