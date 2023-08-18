4e246aaf0716ef01685bf02b807040cfa1182e31
Sean Voeller <sean.voeller@gmail.com>
2023-08-18 10:32:38 -0700

    some notes on what to implement

--- temperature_sensing_alarm/main.py
+++ temperature_sensing_alarm/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+# Build a programmable temperature alarm using the following components:
+#     * Push button
+#         * used to toggle between “program” mode and “monitor” mode
+#     * LCD screen
+#         * “Monitor” mode - display temp and alarm status
+#         * “Program” mode - display “trigger” temp
+#     * Potentiometer
+#         * “Monitor” mode - does nothing
+#         * “Program” mode - used to set the “trigger” temp
+#     * Alarm
+#         * “Monitor” mode - sound the alarm if the trigger temp is reached
+#         * “Program” mode - does nothing
+
+BUTTON_PIN = "FIXME"
+
