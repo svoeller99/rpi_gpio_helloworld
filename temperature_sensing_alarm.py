2d35a361fb248a03252df83b69b555da6fb18f76
Sean Voeller <sean.voeller@gmail.com>
2023-08-18 11:13:29 -0700

    try add_event_detect

--- temperature_sensing_alarm.py
+++ temperature_sensing_alarm.py
@@ -26,15 +26,19 @@ BUTTON_PIN = 6
 LCD_ADDRESS = 0x3f # TODO: verify this via `i2cdetect -y 1`
 ADC_CHANNEL = 0
 
+def handle_button_press():
+    print('button pressed')
+
 GPIO.setmode(GPIO.BCM)
 
 # setup button
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, handle_button_press)
 
 try:
     while True:
-        button_state = GPIO.input(BUTTON_PIN)
-        print(button_state)
+        # button_state = GPIO.input(BUTTON_PIN)
+        # print(button_state)
         sleep(.2)
 except KeyboardInterrupt:
     print('bye')
