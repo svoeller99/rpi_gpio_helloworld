76b5b907cbfd48fc94b088e30429be9f75010491
Sean Voeller <sean.voeller@gmail.com>
2023-08-26 13:27:27 -0700

    keypad test variant using multithreading

--- keypad5_test.py
+++ keypad5_test.py
@@ -5,16 +5,17 @@ import threading
 from keypad import KeyPad
 
 key_pad = KeyPad()
+do_continue = True
 
 def read_from_keypad():
     global key_pad
-    while True:
+    while do_continue:
         try:
             entered_sequence = key_pad.read()
             print(entered_sequence)
         except Exception as e:
             print(e)
-            GPIO.cleanup()
+    GPIO.cleanup()
 
 keypad_thread = threading.Thread(target=read_from_keypad)
 
@@ -23,4 +24,6 @@ try:
     keypad_thread.join()
         
 except KeyboardInterrupt:
+    do_continue = False
+    keypad_thread.join()
     print('bye')
