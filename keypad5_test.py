d6aacf47cba422b967ed486efe0bdb0a694904dc
Sean Voeller <sean.voeller@gmail.com>
2023-08-26 13:30:32 -0700

    keypad test variant using multithreading

--- keypad5_test.py
+++ keypad5_test.py
@@ -2,6 +2,7 @@
 
 import RPi.GPIO as GPIO
 import threading
+import time
 from keypad import KeyPad
 
 key_pad = KeyPad()
@@ -22,7 +23,7 @@ keypad_thread = threading.Thread(target=read_from_keypad)
 try:
     keypad_thread.start()
     while True:
-        pass
+        time.sleep(1)
         
 except KeyboardInterrupt:
     do_continue = False
