d4cbec767d5bdc744a7d758110e8206e2e96d526
Sean Voeller <sean.voeller@gmail.com>
2023-08-27 15:01:57 -0700

    pylint fixes

--- keypad_lcd.py
+++ keypad_lcd.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as GPIO
+"""Small program that reads from a keypad and displays the output on an LCD display."""
+
 import threading
 import time
+from RPi import GPIO
 from keypad import KeyPad
 import LCD1602
 
-stop_event = threading.Event()
+read_from_keypad_stop_event = threading.Event()
 LCD1602.init(0x3f, 1)
 
 def output_to_lcd(output):
+    """Output a string to the LCD display."""
     LCD1602.clear()
     LCD1602.write(0, 0, output)
 
 def read_from_keypad(stop_event):
+    """Continuously read key pad input and display the result on an LCD display."""
     key_pad = KeyPad(stop_event=stop_event)
     while True:
         if stop_event.is_set():
@@ -21,15 +25,15 @@ def read_from_keypad(stop_event):
         entered_sequence = key_pad.read()
         output_to_lcd(entered_sequence)
 
-keypad_thread = threading.Thread(target=read_from_keypad, args=(stop_event,))
+keypad_thread = threading.Thread(target=read_from_keypad, args=(read_from_keypad_stop_event,))
 
 try:
     keypad_thread.start()
     while True:
         time.sleep(.1)
-        
+
 except KeyboardInterrupt:
-    stop_event.set()
+    read_from_keypad_stop_event.set()
     keypad_thread.join()
     print('bye')
 
