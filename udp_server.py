f6c2ca88a8615a1b0662ff7ed02730a29e652525
Sean Voeller <sean.voeller@gmail.com>
2023-09-02 16:49:51 -0700

    initial udp client/server

---
+++ udp_server.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = '127.0.0.1'
+UDP_PORT = 5000
+BUFFER_SIZE = 1024
+
+sock = socket.socket(socket.AF_INET, # IPv4
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    print("received message from %s: %s" % addr, data)
