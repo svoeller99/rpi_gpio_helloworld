66db0fddab5ac6a7ba6b7a50bbbfc18ebe61e7ba
svoeller99 <sean.voeller@gmail.com>
2023-09-09 14:26:50 -0700

    better FPS calculation - display on screen

--- camera.py
+++ camera.py
@@ -3,26 +3,25 @@ from picamera2 import Picamera2
 import time
 
 piCam = Picamera2()
-piCam.preview_configuration.main.size = (1280,720)
+piCam.preview_configuration.main.size = (640,360)
 piCam.preview_configuration.main.format="RGB888"
+piCam.preview_configuration.controls.FrameRate=30
 piCam.preview_configuration.align()
 piCam.configure("preview")
 piCam.start()
 
-prior_second = time.time()
-fps = 0
-current_fps = 0
+fps=30
 
 while True:
+    tStart=time.time()
     frame=piCam.capture_array()
-    current_fps += 1
-    if time.time() - prior_second >= 1:
-        fps = current_fps
-        current_fps = 0
-        prior_second = time.time()
-        print(f"FPS: {fps}")
+    cv2.putText(frame, f"{fps:.1f}", (30,60), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 3)
     cv2.imshow("piCam",frame)
     if cv2.waitKey(1) == ord('q'):
         break
+    tEnd=time.time()
+    elapsed_time = tEnd - tStart
+    fps=.99*fps + .01*(1/elapsed_time)
+    #print(f"{fps:.1f}")
 
 cv2.destroyAllWindows()
