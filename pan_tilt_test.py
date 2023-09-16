c35816b29caaec362f637c12b9731f757847d410
Sean Voeller <sean.voeller@gmail.com>
2023-09-16 12:30:41 -0700

    re-enable pan PWM (but don't test)

--- pan_tilt_test.py
+++ pan_tilt_test.py
@@ -9,10 +9,10 @@ GPIO.setup(tilt_pin, GPIO.OUT)
 GPIO.setup(pan_pin, GPIO.OUT)
 
 tilt_pwm = GPIO.PWM(tilt_pin, 50) # 50 Hz (20 ms PWM period)
-# pan_pwm = GPIO.PWM(pan_pin, 50)
+pan_pwm = GPIO.PWM(pan_pin, 50)
 
 tilt_pwm.start(7.0) # set duty cycle to achieve 90 degrees (0 degrees is 2.0, 180 degrees is 13.0
-# pan_pwm.start(7.0)
+pan_pwm.start(7.0)
 time.sleep(.5)
 
 def change_degrees(pwm, prior_degrees, new_degrees):
@@ -56,7 +56,7 @@ test_tilt(tilt_pwm, change_degrees)
 # test_pan(pan_pwm, change_degrees)
 
 tilt_pwm.ChangeDutyCycle(0)
-# pan_pwm.ChangeDutyCycle(0)
+pan_pwm.ChangeDutyCycle(0)
 tilt_pwm.stop()
-# pan_pwm.stop()
+pan_pwm.stop()
 GPIO.cleanup()
