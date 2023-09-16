7c11085748854893cd92fb237a8f28f6011fa6ca
Sean Voeller <sean.voeller@gmail.com>
2023-09-16 11:51:23 -0700

    temp disable pan pwm

--- pan_tilt_test.py
+++ pan_tilt_test.py
@@ -9,10 +9,10 @@ GPIO.setup(tilt_pin, GPIO.OUT)
 GPIO.setup(pan_pin, GPIO.OUT)
 
 tilt_pwm = GPIO.PWM(tilt_pin, 50) # 50 Hz (20 ms PWM period)
-pan_pwm = GPIO.PWM(pan_pin, 50)
+# pan_pwm = GPIO.PWM(pan_pin, 50)
 
 tilt_pwm.start(7.0) # set duty cycle to achieve 90 degrees (0 degrees is 2.0, 180 degrees is 13.0
-pan_pwm.start(7.0)
+# pan_pwm.start(7.0)
 time.sleep(.5)
 
 def gradually_change_duty_cycle(pwm, prior_degrees, new_degrees):
@@ -40,7 +40,7 @@ for ii in range(0,3):
     time.sleep(1)
 
 tilt_pwm.ChangeDutyCycle(0)
-pan_pwm.ChangeDutyCycle(0)
+# pan_pwm.ChangeDutyCycle(0)
 tilt_pwm.stop()
-pan_pwm.ChangeDutyCycle(0)
+# pan_pwm.ChangeDutyCycle(0)
 GPIO.cleanup()
