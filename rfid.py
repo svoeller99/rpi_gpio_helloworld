65ec0f1a26bce7122457b82e764a07e6a7b43207
Sean Voeller <sean.voeller@gmail.com>
2023-10-01 11:06:52 -0700

    inf loop

--- rfid.py
+++ rfid.py
@@ -7,19 +7,20 @@ from RPi import GPIO
 reader = SimpleMFRC522()
 
 try:
-    command = input("Do you want to write or scan? (write/scan): ")
-    if command == 'write':
-        text = input("Please write the new data: ")
-        print("Please place the card to complete writing")
-        reader.write(text)
-        print("Data writing is complete")
-    elif command == 'scan':
-        print("Reading... Please place the card...")
-        id, text = reader.read()
-        print(f"ID: {id}\nText: {text}")
-        time.sleep(3)
-    else:
-        print(f"Unrecognized command '{command}'")
+    while True:
+        command = input("Do you want to write or scan? (write/scan): ")
+        if command == 'write':
+            text = input("Please write the new data: ")
+            print("Please place the card to complete writing")
+            reader.write(text)
+            print("Data writing is complete")
+        elif command == 'scan':
+            print("Reading... Please place the card...")
+            id, text = reader.read()
+            print(f"ID: {id}\nText: {text}")
+            time.sleep(3)
+        else:
+            print(f"Unrecognized command '{command}'")
 except KeyboardInterrupt:
     print('bye')
 GPIO.cleanup()
