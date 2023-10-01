6f96a9a2825ce8f4b06af56ff865a634b3a46bc5
svoeller99 <sean.voeller@gmail.com>
2023-10-01 15:48:26 -0700

    walk through LEDs

--- bar_graph.py
+++ bar_graph.py
@@ -8,11 +8,14 @@ ledPins = [11, 12, 13, 15, 16, 18, 22, 3, 5, 24]
 GPIO.setmode(GPIO.BOARD)
 for i in ledPins:
     GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, GPIO.LOW)
-
-for i in ledPins:
     GPIO.output(i, GPIO.HIGH)
-    time.sleep(.5)
-    GPIO.output(i, GPIO.LOW)
 
+try:
+    while True:
+        for i in ledPins:
+            GPIO.output(i, GPIO.LOW)
+            time.sleep(.5)
+            GPIO.output(i, GPIO.HIGH)
+except KeyboardInterrupt:
+    print('bye')
 GPIO.cleanup()
